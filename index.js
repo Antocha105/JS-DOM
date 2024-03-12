0e4a3029fc4f04c06e16c20d061fb54ca46416e1
Chuprin Anton <workchuprin105@gmail.com>
2024-03-12 22:06:17 +0200

    practise dom

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getAlert(){
 const button = document.getElementById('button')
 
 button.addEventListener('click',getAlert)
+
+
+
+
+const btn = document.getElementById('btn')
+
+btn.addEventListener('mouseentr',getAlert)
