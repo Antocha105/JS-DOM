20e8c8c2a743db149e831de5f196503d767f19b0
Chuprin Anton <workchuprin105@gmail.com>
2024-03-12 22:48:07 +0200

    Remove event lisener

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ button.addEventListener('click',getAlert)
 const btn = document.getElementById('btn')
 
 btn.addEventListener('mouseentr',getAlert)
+
+const btnNo = document.getElementById('no-btn')
+
+btnNo.addEventListener('mouseenter',function(){
+    btnNo.style.display = 'none';
+})
+
+btnNo.addEventListener('mouseleave',function(){
+    btnNo.style.display = 'block';
+})
