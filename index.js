6feb7ca75ffdedd9789595a9eef6b4bd73a56db3
Chuprin Anton <workchuprin105@gmail.com>
2024-03-16 17:25:43 +0200

    Class list

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
+const div = document.querySelector('#box')
 
-const CURRENCY ={
-    USD_TO_UAN: 37.65,
-    EUR_TO_UAN: 40.61
+
+const [red,green] = document.querySelectorAll('.btn')
+
+red.addEventListener('click',redBtnHandler)
+
+function redBtnHandler(event){
+    //div.style.backgroundColor = 'red'
+    div.classList.add('bg-color-red')
 }
 
-const converterForm = document.querySelector('#currency-converter-form')
+green.addEventListener('click', greenBtnHandler)
 
-converterForm.addEventListener('submit',convertHandler);
-
-function convertHandler(event){
-    event.preventDefault();
-
-    const amount = Number(document.querySelector('#amount').value);
-
-    const currency = document.querySelector('#currency').value;
-    
-    let convertedAmount;
-
-    if(currency===USD){
-        convertedAmount = amount*currency.USD_TO_UAN;
-    }else if(currency===EUR){
-        convertedAmount = amount*currency.EUR_TO_UAN;
-    }else{
-        throw new Error('Something problem...!')
-    }
-
-    const resultDiv = document.querySelector('#result');
-    resultDiv.innerHTML = `${amount} ${currency} = ${convertedAmount.toFixed(2)} UAH`
-
+function greenBtnHandler(event){
+   // div.style.backgroundColor = 'green'
+   div.classList.add('bg-color-green')
 }
+
