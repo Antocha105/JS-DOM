2715f708cbedb86dfeb2b9ee356d525c08642dda
Chuprin Anton <workchuprin105@gmail.com>
2024-03-16 12:55:34 +0200

    getElements by class name and id

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const form = document.getElementById('name-form')
+const collection = document.getElementsByClassName('paragraph')
 
-form.addEventListener('submit',consoleForm)
+//for(let i=0; i<collection.lenght; i++){
+//    collection[i].style.color = 'green';
+//}
 
-function consoleForm(event){
-    event.preventDefault()
-    
+//for...of - цикл що перебирає обьект по ключам
 
-    const form = event.target;
-    const value = form[0].value
-    console.log(`Hello  ${value}`)
+for(let p of collection){
+    p.style.color = 'green'
+}
+
+const buttons=document.getElementsByTagName('button')
+const arr = [...buttons]
+console.log(buttons)
+
+arr.forEach((button)=>{
+    button.style.color = 'red'
+})
+
+function hello(event){
+    console.log('Helloworld')
+    event.target.addEventListener
+
+}
+
+for(let btn of buttons){
+    btn.addEventListener('click', hello)
 }
