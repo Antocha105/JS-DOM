d7e7ac7a90984c8dd6cb37be5b781a72e6e36c32
Chuprin Anton <workchuprin105@gmail.com>
2024-03-24 17:28:36 +0200

    User card

---
+++ data.js
@@ -0,0 +1,43 @@
+const data = [
+    {
+        id: 1,
+        name: 'John',
+        description: 'Description for John',
+        profilePicture: 'https://illustrators.ru/uploads/illustration/image/1705348/IMG_20221224_003828_372.jpg'
+    }, {
+        id: 2,
+        name: 'Josh',
+        description: 'Description for Josh',
+        profilePicture: 'https://illustrators.ru/uploads/illustration/image/1705348/IMG_20221224_003828_372.jpg'
+    }, {
+        id: 3,
+        name: 'Jane',
+        description: 'Description for Jane',
+        profilePicture: 'https://illustrators.ru/uploads/illustration/image/1705348/IMG_20221224_003828_372.jpg'
+    }, {
+        id: 4,
+        name: 'Iness',
+        description: 'Description for Iness',
+        profilePicture: 'https://illustrators.ru/uploads/illustration/image/1705348/IMG_20221224_003828_372.jpg'
+    }, {
+        id: 5,
+        name: 'Alex',
+        description: 'Description for Alex',
+        profilePicture: 'https://illustrators.ru/uploads/illustration/image/1705348/IMG_20221224_003828_372.jpg'
+    }, {
+        id: 6,
+        name: 'Nathan',
+        description: 'Description for Nathan',
+        profilePicture: 'https://illustrators.ru/uploads/illustration/image/1705348/IMG_20221224_003828_372.jpg'
+    }, {
+        id: 7,
+        name: 'Tracy',
+        description: 'Description for Tracy',
+        profilePicture: 'https://illustrators.ru/uploads/illustration/image/1705348/IMG_20221224_003828_372.jpg'
+    }, {
+        id: 8,
+        name: 'James',
+        description: 'Description for James',
+        profilePicture: 'https://illustrators.ru/uploads/illustration/image/1705348/IMG_20221224_003828_372.jpg'
+    }
+];
--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-const div = document.querySelector('#box')
+/*<article class="card-wrapper">
+      <img class="card-image" src="https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg" alt="John avatar">
+      <h2 class="user-name">John</h2>
+      <p class="deccription">Deskription for John</p>
+    </article>
+    */
 
+const johnObject = {
+        id: 1,
+        name: 'John',
+        description: 'Description for John',
+        profilePicture: 'https://illustrators.ru/uploads/illustration/image/1705348/IMG_20221224_003828_372.jpg'
+    }
 
-const [red,green] = document.querySelectorAll('.btn')
+const root = document.querySelector('#root')
 
-red.addEventListener('click',redBtnHandler)
+function createUserCard(user){
 
-function redBtnHandler(event){
-    //div.style.backgroundColor = 'red'
-    div.classList.add('bg-color-red')
+    const article = document.createElement('article');
+    article.classList.add('card-wrapper')
+
+    const img = document.createElement('img')
+    img.setAttribute('src',user.profilePicture)
+    img.setAttribute('alt',user.name)
+    img.classList.add('card-image')
+
+    const h2 = document.createElement('h2')
+    h2.append(user.name)
+    h2.classList.add('username')
+
+    const p = document.createElement('p')
+    p.append(user.description)
+    p.classList.add('description')
+
+    article.append(img, h2, p)
+
+    
+    return article;
 }
 
-green.addEventListener('click', greenBtnHandler)
+const cardArray = data.map(user => createUserCard(user));
 
-function greenBtnHandler(event){
-   // div.style.backgroundColor = 'green'
-   div.classList.add('bg-color-green')
-}
-
+root.append(...cardArray);
