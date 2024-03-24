7acc73627aea5145c0b6b1747bf0184faf6c3004
Chuprin Anton <workchuprin105@gmail.com>
2024-03-24 18:10:11 +0200

    Refactor

--- index.js
+++ index.js
@@ -16,21 +16,27 @@ const root = document.querySelector('#root')
 
 function createUserCard(user){
 
-    const article = document.createElement('article');
-    article.classList.add('card-wrapper')
+    //const article = document.createElement('article');
+    //article.classList.add('card-wrapper')
+    
+    const article = createElement('article',{classNames:['card-wrapper']})
 
     const img = document.createElement('img')
     img.setAttribute('src',user.profilePicture)
     img.setAttribute('alt',user.name)
     img.classList.add('card-image')
 
-    const h2 = document.createElement('h2')
-    h2.append(user.name)
-    h2.classList.add('username')
+    //const h2 = document.createElement('h2')
+    //h2.append(user.name)
+    //h2.classList.add('username')
 
-    const p = document.createElement('p')
-    p.append(user.description)
-    p.classList.add('description')
+    const h2 = createElement('h2',{classNames:['username']},user.name)
+
+    //const p = document.createElement('p')
+    //p.append(user.description)
+    //p.classList.add('description')
+
+    const p = createElement('p',{classNames:['description']},user.description)
 
     article.append(img, h2, p)
 
@@ -41,3 +47,21 @@ function createUserCard(user){
 const cardArray = data.map(user => createUserCard(user));
 
 root.append(...cardArray);
+
+/**
+ * @param {String} type - тег елемента, який нам треба створити
+ * @param {String[]} classNames - список класів, які треба додати до елемента
+ * @param {...Node} childNodes - список дочірніх вузлів
+ * @returns {HTMLElement}
+ */
+
+
+
+function createElement(type,{classNames},...childNodes){
+    const elem = document.createElement(type)
+    elem.classList.add(...classNames)
+    elem.append(...childNodes)
+
+    return elem
+
+}
