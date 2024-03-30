1539e0d914dfc89d84084284019deca78ff05f40
Chuprin Anton <workchuprin105@gmail.com>
2024-03-30 19:30:25 +0200

    add timeout

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 function loadImage(src){
+
     const img = document.createElement('img')
     img.setAttribute('src',src)
 
     const h2 = document.createElement('h2')
     h2.id = 'loading-h2'
-    h2.append.apply('Loading image ....')
-    document.body.append(h2)
-
+    h2.append('Loading image....')
+    document.body.append(h2)  
+    
     return new Promise((resolve,reject)=>{
+
+        const timeOutId = setTimeout(()=>{
+            reject('Image can not be loaded')
+        },5000)
+    
+    
         img.addEventListener('load',()=>{
+            clearTimeout(timeOutId)
             resolve(img)
         });
-
+    
         img.addEventListener('error',()=>{
+            clearTimeout(timeOutId)
             reject('Image can not be loaded')
         })
     })
-
-
 }
 
-loadImage('./завантаження.jpeg')
-.then((img)=>{
+
+
+loadImage('./завантаження.jpeg').then((img)=>{
     document.body.append(img);
 },(errorMessage)=>{
     const h2 = document.createElement('h2');
