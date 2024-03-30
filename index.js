2adda68d35b03f841dd277f92d87b579cc75f7aa
Chuprin Anton <workchuprin105@gmail.com>
2024-03-30 16:42:20 +0200

    Then Catsh Finally

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 45,
-    footSize: 45,
-// Серіалізація - процес перетворення JS об'єкта в JSON
-// Десеріалізація - процес перетворення JSON в JS об'єкт
+const p = fetch('./user.json')
 
-// Поверхнева копія об'єкту (shallow copy) - копія, де копіюються тільки посилання на елементи, а не самі значення
+const jsonPromise = p.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((err)=>{
+    console.log(err)
+})
+.finally(()=>{
+    console.log('FINALLY queshion closed')
+})
 
-const originalObject = {
-    users: [
-        {
-            name: 'John',
-            age: 25
-        },
-        {
-            name: 'Lucky',
-            age: 33
-        }
-    ]
-}
-
-// JSON
-const shallowCopy = {...originalObject};
-
-
-// stringify - перетворює JS об'єкт в JSON
-// parse - перетворює JSON в JS об'єкт
-// Глибока копія (deep copy) - створення повноцінної копії об'єкта, де копіюються як посилання на його елементи, так і їхні значення
-
-const jsonString = JSON.stringify(user);
-const deepCopy = JSON.parse(JSON.stringify(originalObject));
