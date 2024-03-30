adc6504acf69340f86efffd81fb28ed114199dd0
Chuprin Anton <workchuprin105@gmail.com>
2024-03-30 18:39:04 +0200

    promisify loading image

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const p = new Promise(executor);
+function loadImage(src){
+    const img = document.createElement('img')
+    img.setAttribute('src',src)
+
+    const h2 = document.createElement('h2')
+    h2.id = 'loading-h2'
+    h2.append.apply('Loading image ....')
+    document.body.append(h2)
+
+    return new Promise((resolve,reject)=>{
+        img.addEventListener('load',()=>{
+            resolve(img)
+        });
+
+        img.addEventListener('error',()=>{
+            reject('Image can not be loaded')
+        })
+    })
 
 
-function executor(resolve, reject){
-    setTimeout(()=>{
-        resolve('It time')
-    },15000)
 }
 
-const h1 = document.querySelector('#root')
-
-p.then((str)=>{
-    h1.append(str)
+loadImage('./завантаження.jpeg')
+.then((img)=>{
+    document.body.append(img);
+},(errorMessage)=>{
+    const h2 = document.createElement('h2');
+    h2.append(errorMessage)
+    document.body.append(h2)
+    
+})
+.finally(()=>{
+    const loadingH2= document.querySelector('#loading-h2');
+    loadingH2.remove();
 })
