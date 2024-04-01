adb5cf6505b264bd93b750efeb8e84e42e110bef
Chuprin Anton <workchuprin105@gmail.com>
2024-04-01 22:54:16 +0300

    Async AWAIT

--- index.js
+++ index.js
@@ -1,84 +1,27 @@
- //https://api.openweathermap.org/data/2.5/weather?q=Dnipro&appid=f7c576ba3699bdd0b98ddcf196639992&units=metric
+//async
+//await
+
+const URL = 'https://randomuser.me/api'
 
 
 
-const API_BASE = 'https://api.openweathermap.org/data/2.5/weather';
 
-const API_KEY = 'f7c576ba3699bdd0b98ddcf196639992';
+function loadDataWithoutAsync(url){
+    const p = fetch(url);
 
-const btn = document.querySelector('.btn')
-
-btn.addEventListener('click', buttonClickHandler)
-
-function buttonClickHandler({target}){
-
-    const selectValue = target.previousElementSibling.value;
-
-    requestApi(selectValue)
-    
-
-}
-
-function requestApi(cityName){
-    //готуємо URL
-    const url = `${API_BASE}?q=${cityName}&appid=${API_KEY}&units=metric`
-
-   
-
-    fetch(url)
-    .then((response)=>{
-        return response.json()
-    })
-    .then((data)=>{
-        displayWeather(data)
+    const jsonResponse = p.then((response)=>{
+        return response.json();
     })
 
+    jsonResponse.then((data)=>{
+        console.log(data);
+    })
 }
 
-function displayWeather (weatherObject){
+//ASYNC/AWAIt
 
-    //деструктуризація обектів
-    const{name,main:{temp}, weather:[{description}]}=weatherObject;
-
-    const article = document.querySelector('#weather-box');
-    article.classList.add('weather-display')
-
-    const city = document.querySelector('#city')
-    city.textContent = name;
-
-    const teperature = document.querySelector('#temp');
-    temperature.textContent = `${temp}°C`
-
-    const desc = document.querySelector('#description');
-    desc.textContent = description;
-
-    
-    //const article = document.createElement('article')
-    //article.classList.add('weather')
-
-    //const cityName = document.createElement('p')
-    //cityName.append(`City name: ${name}`);
-
-    //const teperature = document.createElement('p')
-    //teperature.append(`Temperature: ${temp}°C`);
-
-    //const weatherDescription = document.createElement('p')
-    //weatherDescription.append(`Weather description: ${description}`)
-
-    //article.append(cityName, teperature, weatherDescription);
-
-    //const section = document.querySelector('.wrapper')
-    //section.append(article)
+async function loadDataAsync(url){
+    const p = fetch(URL);
+    const response = await p;
+    const data = await response.json()
 }
-
-/*
-<article class="weather">
-      <p>City name:Kyiv</p>
-      <p>Temperatyre: 7</p>
-      <p>Weather description</p>
-</article>
-*/
-
-
-
-
